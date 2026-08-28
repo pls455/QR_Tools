2c5e52c0d6db8af8d63188db9a3208d6c3980de3
pls455 <karamhahhss123@gmail.com>
2026-08-28 12:51:11 +0300

    refactor: add application composition root

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Application composition root.
+// Features should depend on core/services rather than each other directly.
+export * from "./core/firebase.js";
+export * from "./core/roles.js";
+export * from "./utils/dom.js";
+export * from "./utils/strings.js";
